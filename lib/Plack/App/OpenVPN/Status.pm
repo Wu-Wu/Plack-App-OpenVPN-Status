319c333f17f7a59e66331cc7fc7bf739b4733250
Anton Gerasimov <me@zyxmasta.com>
2012-12-04 05:09:12 +0400

    updated Makefile.PL

--- lib/Plack/App/OpenVPN/Status.pm
+++ lib/Plack/App/OpenVPN/Status.pm
@@ -1,52 +1,53 @@
 package Plack::App::OpenVPN::Status;
 
+# ABSTRACT: Plack application to display the sessions of OpenVPN server
+
 use 5.008009;
 use strict;
 use warnings;
 
-our @ISA = qw();
+use parent 'Plack::Component';
+use Carp ();
 
 our $VERSION = '0.1.1';
 
+#
+#
+sub prepare_app {
 
-# Preloaded methods go here.
+}
+
+#
+#
+sub call {
+    my ($self, $env) = @_;
+
+}
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Plack::App::OpenVPN::Status - Perl extension for blah blah blah
+Plack::App::OpenVPN::Status - Plack application to display the sessions of OpenVPN server
 
 =head1 SYNOPSIS
 
   use Plack::App::OpenVPN::Status;
-  blah blah blah
 
 =head1 DESCRIPTION
 
-Stub documentation for Plack::App::OpenVPN::Status, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
+to be written..
 
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+L<Plack>
 
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
 
 =head1 AUTHOR
 
-Anton Gerasimov, E<lt>chim@zyxmasta.comE<gt>
+Anton Gerasimov, E<lt>me@zyxmasta.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
